16182d26d73e02be7fd10edbf7c46c302c39d237
Huucong95 <87304808+Huucong95@users.noreply.github.com>
2023-04-02 20:48:21 +0700

    Update chat.jsx

--- components/chat.jsx
+++ components/chat.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const FacebookChat = () => {
   useEffect(() => {
     // Load Facebook SDK script
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
       FB.init({
-        xfbml            : true,
-        version          : 'v12.0'
+        xfbml: true,
+        version: "v12.0",
       });
     };
 
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
+      js = d.createElement(s);
+      js.id = id;
       js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
       fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    })(document, "script", "facebook-jssdk");
   }, []);
 
   return (
-    <div className="fb-customerchat"
+    <div
+      className="fb-customerchat"
       attribution="setup_tool"
-      page_id="100076087988203"
+      page_id="107823518650991"
       logged_in_greeting="Hi! How can we help you?"
-      logged_out_greeting="Hi! How can we help you?">
-    </div>
+      logged_out_greeting="Hi! How can we help you?"
+    ></div>
   );
 };
 
